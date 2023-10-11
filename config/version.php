ef07535aeace510cf0cd9a47aa1efadf9e9fccb5
indazzz <luthfus.inasshaliha@gmail.com>
2023-10-11 08:04:46 +0700

    origin app

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.2.2',
+  'full_app_version' => 'v6.2.2 - build 11714-ga95fae0e9',
+  'build_version' => '11714',
+  'prerelease_version' => '',
+  'hash_version' => 'ga95fae0e9',
+  'full_hash' => 'v6.2.2-85-ga95fae0e9',
+  'branch' => 'master',
+);
